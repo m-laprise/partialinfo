fix bugs in LRMC data generation and masking; finish setting up LRMC task; remove return mode for DGP

src/datautils/datagen_temporal.py
@@ -250,11 +250,6 @@ def _generate_U(vcovsU: torch.Tensor, *, offset: float = 0.0):
     return U_rt.T  # shape: (t, r)
 
 
-@torch.no_grad()
-def _fin_return(val_start, val_end):
-    return (val_end - val_start) / val_start
-
-
 class GTMatrices(Dataset):
     """
     Generates ground truth matrices. Task-agnostic and does not contain any labels.
@@ -272,26 +267,27 @@ class GTMatrices(Dataset):
                  U_only: bool = False,
                  seed: Optional[int] = None,
                  **kernel_kwargs):
-        assert N % realizations == 0
         super().__init__()
         if seed is not None:
             torch.manual_seed(seed)
-        self.mode = mode
-        self.kernel = kernel
-        self.vtype = vtype
         self.N = N
-        self.num_dgps = N // realizations
-        self.U_only = U_only
-        if self.U_only:
-            if r < m:
-                print(f"WARNING: U_only option is activated but rank {r} was requested.",
-                    "Full rank matrices will be returned.")
-                r = m
         self.t, self.m, self.r = t, m, r
-        self.structured = structured
-        self.realizations = realizations
         #self.sigma = sigma
-        self.U, self.V, self.vcovU = self._generate_factors(N, **kernel_kwargs)
+        self.structured = structured
+        if self.structured:
+            assert N % realizations == 0
+            self.realizations = realizations
+            self.mode = mode
+            self.kernel = kernel
+            self.vtype = vtype
+            self.num_dgps = N // realizations
+            self.U_only = U_only
+            if self.U_only:
+                if r < m:
+                    print(f"WARNING: U_only option is activated but rank {r} was requested.",
+                        "Full rank matrices will be returned.")
+                    r = m
+        self.U, self.V, self.vcovU = self._generate_factors(self.N, **kernel_kwargs)
 
     def __len__(self):
         return self.U.shape[0]
@@ -299,8 +295,6 @@ class GTMatrices(Dataset):
     def _generate_factors(self, N: int, **kernel_kwargs) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
         if self.structured:
             t = self.t
-            if self.mode == 'return':
-                t += 1
             vcovsUs = torch.zeros((self.num_dgps, self.r, t, t))
             Us = torch.zeros((self.realizations * self.num_dgps, t, self.r))
             Vs = torch.zeros((self.realizations * self.num_dgps, self.m, self.r))
@@ -316,7 +310,8 @@ class GTMatrices(Dataset):
                     Us[idx] = _generate_U(vcovsU)
                     Vs[idx] = V
         else:
-            Us = torch.rand(N, self.t, self.r, dtype=torch.float32) * 2
+
+            Us = torch.rand(N, self.t, self.r, dtype=torch.float32)
             Vs = torch.rand(N, self.m, self.r, dtype=torch.float32)
             vcovsUs = torch.eye(self.t).reshape(1, 1, self.t, self.t)
         return Us, Vs, vcovsUs
@@ -324,24 +319,19 @@ class GTMatrices(Dataset):
     def generate_matrices(self, idx = None):
         idx = np.arange(self.U.shape[0]) if idx is None else idx
         
-        if self.U_only:
-            U = self.U[idx]
-            for i in range(U.shape[0]):
-                U[i] /= U[i].std(dim=0)
-            return U
+        if self.structured:
+            if self.U_only:
+                U = self.U[idx]
+                for i in range(U.shape[0]):
+                    U[i] /= U[i].std()
+                return U
         
         M = torch.einsum('...ij,...kj->...ik', self.U[idx], self.V[idx])
         #M += self.sigma * torch.randn(M.shape, dtype=torch.float32)
-        if not self.structured:
-            M /= np.sqrt(self.r)
-            return M / M.std(dim=1, keepdim=True)
-        elif self.mode == 'return':
-            M_r = _fin_return(M[:, :-1, :], M[:, 1:, :])
-            return M_r / M_r.std(dim=1, keepdim=True)
-        else:
-            for i in range(M.shape[0]):
-                M[i] /= M[i].std(dim=0)
-            return M
+        for i in range(M.shape[0]):
+            M[i] /= M[i].std()
+        return M
+            
         
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
@@ -373,7 +363,7 @@ class TemporalData(Dataset):
     be provided to a DataLoader.
     """
     def __init__(self, matrices: GTMatrices, task: str = 'argmax', *, 
-                 target_source: str = 'observed', verbose=True, rho_out: float = 0.4):
+                 target_source: str = 'observed', verbose = True, rho_out: float = 0.4):
         super().__init__()
         if task not in ['argmax', 'lastrow', 'nextrow', 'nonlin_function', 'lrmc']:
             raise NotImplementedError(f"Task {task} not implemented")

src/utils/plotting.py
@@ -10,7 +10,7 @@ from matplotlib.ticker import MaxNLocator
 def plot_stats(stats, filename_base, true_nuclear_mean, true_gap_mean, true_variance):
     epochs = np.arange(1, len(stats["train_loss"]) + 1)
     # Plot loss-related metrics in two panels
-    fig, axs = plt.subplots(2, 1, figsize=(10, 12), dpi=320)
+    fig, axs = plt.subplots(1, 3, figsize=(12, 5), dpi=320)
     if min(stats["train_loss"]) >= 0:
         axs[0].plot(epochs, np.log(stats["train_loss"]), label="Train Loss", color='tab:blue')
     else:
@@ -29,23 +29,29 @@ def plot_stats(stats, filename_base, true_nuclear_mean, true_gap_mean, true_vari
                 label="Train MSE Known Entries", color='tab:blue')
     axs[1].plot(epochs, stats["t_mse_unknown"], 
                 label="Train MSE Unknown Entries", color='tab:purple')
-    axs[1].plot(epochs, stats["t_variance"],
-                label="Train Var between Agents", color='tab:grey')
     #axs[1].axhline(y=true_variance, label="Ref Var of Entries", 
     #               color='tab:grey', linestyle='--')
     axs[1].plot(epochs, stats["val_mse_known"], linestyle='dotted', 
                 label="Val MSE Known Entries", color='tab:green')
     axs[1].plot(epochs, stats["val_mse_unknown"], linestyle='dotted', 
                 label="Val MSE Unknown Entries", color='tab:orange')
-    axs[1].plot(epochs, stats["val_variance"], linestyle='dotted', 
-                label="Val Var between Agents", color='tab:red')
-    axs[1].set_title("Training & Validation MSE and Diversity")
+    axs[1].set_title("Training & Validation MSE")
     axs[1].set_xlabel("Epoch")
     axs[1].set_ylabel("")
     axs[1].grid(True)
     #axs[1].set_ylim(0, 2)
     axs[1].legend()
     axs[1].xaxis.set_major_locator(MaxNLocator(integer=True))
+    axs[2].plot(epochs, stats["t_variance"],
+                label="Train Var between Agents", color='tab:grey')
+    axs[2].plot(epochs, stats["val_variance"], linestyle='dotted', 
+                label="Val Var between Agents", color='tab:red')
+    axs[2].set_title("Training & Validation Diversity")
+    axs[2].set_xlabel("Epoch")
+    axs[2].set_ylabel("")
+    axs[2].grid(True)
+    axs[2].legend()
+    axs[2].xaxis.set_major_locator(MaxNLocator(integer=True))
     fig.tight_layout()
     fig.savefig(f"{filename_base}_loss_metrics.png")
     plt.close(fig)

src/utils/training_temporal.py
@@ -4,6 +4,7 @@ from typing import Optional
 
 import torch
 import torch.nn as nn
+from geomloss import SamplesLoss
 from torch.amp.autocast_mode import autocast
 from torch.amp.grad_scaler import GradScaler
 
@@ -138,37 +139,44 @@ def spectral_penalty_batched(predictions, rank, eps=1e-6, gamma=1.0):
         # possible for tall or thin matrices
         S = torch.linalg.svdvals(predictions)  # shape: [batch_size, min(n, m)]
     except RuntimeError:
-        return torch.tensor(0.0, device=predictions.device)
+        return torch.tensor(0.0, device=predictions.device), 0.0, 0.0
+    
     # sum of singular values from index 'rank' onward
     sum_rest = S[:, rank:].sum(dim=1) 
-    s_max = S[:, 0] 
+    #s_max = S[:, 0] 
     N = torch.tensor(min(predictions.shape[1], predictions.shape[2]), 
                      device=predictions.device, dtype=predictions.dtype)
-
-    soft_upper = torch.relu(s_max - 2 * N)
-    soft_lower = torch.relu((N // 2) - s_max)
-    range_penalty = soft_upper**2 + soft_lower**2 
-
-    penalty = sum_rest / (N - rank) + gamma * range_penalty
+    #soft_upper = torch.relu(s_max - 2 * N)
+    #soft_lower = torch.relu((N // 2) - s_max)
+    #range_penalty = soft_upper**2 + soft_lower**2 
+    penalty = sum_rest / (N - rank) #+ gamma * range_penalty
     # spectral gap between rank-th and (rank+1)-th singular value for each sample
     gap = S[:, rank - 1] - S[:, rank]
-    return penalty.mean(), gap.mean(), S.sum(dim=1).mean().item()
+    return penalty.mean(), gap.mean().item(), S.sum(dim=1).mean().item()
 
 
 def penalized_MSE(predictions: torch.Tensor,
                   targets: torch.Tensor,
                   reduction: str = 'mean', *,
-                  rank: int = 1, theta: float = 0.5):
+                  rank: int = 1, theta: float = 0.95, globalmask: torch.Tensor):
     B, A, y_dim = predictions.shape
     # NOTE: to change for rectangular implementation
     n = int(y_dim ** 0.5)
-    error = stacked_MSE(predictions, targets, reduction=reduction)
+    maskedpreds = predictions[globalmask.expand_as(predictions)].view(B, A, -1)
+    maskedtargets = targets[globalmask.expand_as(targets)].view(B, -1)
+    error = stacked_MSE(maskedpreds, 
+                        maskedtargets, reduction=reduction)
+    # Wasserstein-2 OT distance with small epsilon
+    ot = SamplesLoss("sinkhorn", p=2, blur=0.01)(
+            maskedpreds.mean(dim=1), 
+            maskedtargets
+    ) / 20
     penalty, _, _ = spectral_penalty_batched(predictions.reshape(B * A, n, n), rank)
-    return theta * error + (1 - theta) * penalty
+    return (theta * error + (1 - theta) * penalty + ot) /5
 
 
 def train(model, aggregator, loader, optimizer, criterion, criterion_kwargs, device, 
-          scaler: Optional[GradScaler], *, t, m):
+          scaler: Optional[GradScaler], *, t, m, globalmask, task_cat):
     model.train()
     if aggregator is not None:
         aggregator.train()
@@ -625,5 +633,7 @@ def final_test(model, aggregator, test_loader, globalmask, criterion, criterion_
             task_cat=task_cat, t=cfg.t, m=cfg.m, global_mask=globalmask
         )
         test_stats = (t_loss, t_mse_known, t_mse_unknown, t_variance, t_penalty, t_gap, t_nucnorm)
+        print("Test Set Performance | ",
+              f"Loss: {t_loss:.4f}, MSE known: {t_mse_known:.4f}, MSE unknown: {t_mse_unknown:.4f}, Penalty: {t_penalty:.4f}, Gap: {t_gap:.4f}, NucNorm: {t_nucnorm:.4f}")
 
     return test_stats